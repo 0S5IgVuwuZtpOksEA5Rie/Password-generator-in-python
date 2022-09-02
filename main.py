c5ff132cb304a55725b19623bee123c5ea899435
0S5IgVuwuZtpOksEA5Rie <3qzwztfvcyvx6v@gmail.com>
2022-09-02 11:40:31 +1000

    Only can insert valid inputs

--- main.py
+++ main.py
@@ -1,30 +1,73 @@
 import random
 
+# loop as long as the user wants to run it
 run = True
 while run:
+    # list of characters available
     characters = '''1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_\=+`~!@#$%^&*()[]{}|;:'",.<>/?'''
 
+    # list of characters available but only the numbers
     pin = '1234567890'
 
-    weatherPinOrNot = input('pin(y/n): ')
+    correctDatatype = False
+    while not correctDatatype:
+        # use alphanumeric characters or just numbers
+        weatherPinOrNot = input('pin(y/n): ')
 
-    if weatherPinOrNot == 'y':
-        characters = pin
+        if weatherPinOrNot == 'y':
+            # override to just numbers
+            characters = pin
+            correctDatatype = True
+        elif weatherPinOrNot == 'n':
+            correctDatatype = True
+        else:
+            print('\nInvalid input\n')
+            correctDatatype = False
 
-    lengthOfPassword = input('length of password: ')
+    # get settings
+    correctDatatype = False
+    while not correctDatatype:
+        lengthOfPassword = input('length of password: ')
 
-    howManyPasswords = input('how many passwords: ')
+        if type(lengthOfPassword) == int:
+            correctDatatype = True
+        else:
+            print("\nInvalid input\n")
+            correctDatatype = False
 
+    correctDatatype = False
+    while not correctDatatype:
+        howManyPasswords = input('how many passwords: ')
+
+        if type(correctDatatype) == int:
+            correctDatatype = True
+        else:
+            print("\nInvalid input\n")
+            correctDatatype = False
+
+    # for every password
     for weatherPinOrNot in range(int(howManyPasswords)):
+        # create password storage
         password = ''
+
+        # for every character
         for i in range(int(lengthOfPassword)):
             character = random.choice(characters)
+
             password += character
+
         print(password)
 
-    rerun = input('\n' + 'redo(y/n): ')
+    correctDatatype = False
+    while not correctDatatype:
+        rerun = input('\nredo(y/n): ')
 
-    if rerun == 'n':
-        run = False
-    else:
-        run = True
+        if rerun == 'y':
+            correctDatatype = True
+            run = True
+        if rerun == 'n':
+            correctDatatype = True
+            run = False
+        else:
+            print('\nInvalid input\n')
+            correctDatatype = False
